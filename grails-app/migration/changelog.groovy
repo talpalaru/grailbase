0d29933fe4cd5feaf8fe5f7d4a85e1cebdd6721f
tami <Michael.Talpalaru@dvinci.de>
2017-09-07 12:10:57 +0200

    External Config

---
+++ grails-app/migration/changelog.groovy
@@ -0,0 +1,22 @@
+//noinspection GroovyUnusedAssignment
+databaseChangeLog = {
+
+    changeSet(author: 'tami', id: ' 1 ') {
+        createTable(tableName: "tenant") {
+            column(autoIncrement: "true", name: "id", type: "bigint") {
+                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "PK_TENANT")
+            }
+
+            column(name: "version", type: "bigint") {
+                constraints(nullable: "false")
+            }
+            column(name: "version", type: "bigint") {
+                constraints(nullable: "false")
+            }
+            column(name: "name", type: "varchar(1000)") {
+                constraints(nullable: "false")
+           }
+        }
+    confirm ' DB changes succesfully done.'
+    }
+}
